69b943c05cd6a7adec8daccf4a116b06c8d5559c
Mauro Jutzutz Sepet <mauro.jutzutz@gmail.com>
2021-04-19 23:39:47 -0600

    prueba

--- src/config/config.keys.ts
+++
@@ -1,9 +0,0 @@
-export enum Configuration {
-  PORT = 'PORT',
-  HOST = 'HOST',
-  USERNAME = 'USERNAME',
-  PASSWORD = 'PASSWORD',
-  DATABASE = 'DATABASE',
-  PORT_DB = '1433',
-  JWT_SECRET = 'JWT_SECRET',
-}
