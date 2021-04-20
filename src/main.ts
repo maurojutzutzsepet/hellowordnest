5ee2a3047e8a0b73c027006dc29b7b49f874a54b
Mauro Jutzutz Sepet <mauro.jutzutz@gmail.com>
2021-04-19 23:12:42 -0600

    prueddbswagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('BACKEND DIACO')
+    .setDescription('SERVICIO DIACO API  MODULO QUEJAS')
+    .setVersion('1.0')
+    .addTag('Mauro jutzutz 1990-14-12114')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
